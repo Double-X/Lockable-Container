932de9a9e56c0087edee84544a5e29383bc0bf82
Double-X <lkllun@yahoo.com.hk>
2017-02-11 10:58:38 +0800

    Create lockableContainerClass.js

---
+++ js/class/lockableContainerClass.js
@@ -0,0 +1,52 @@
+function LockableContainerClass(CALLBACK, ERRBACK, KEY) {
+
+    'use strict';
+
+    var _MSG_HAS_CONTENTS = "This container already has contents";
+    var _MSG_INCORRECT_KEY = "The key doesn't match the lock of this container";
+    var _MSG_LOCKED = 'This container is locked';
+    var _MSG_NO_CONTENTS = "This container has no contents";
+    var _NULL_CONTENTS = {};
+
+    var _contents = _NULL_CONTENTS, _isLocked = false;
+    var _protected = {
+        isCorrectKey: function(key) { return key === KEY; },
+        unlock: function() { _isLocked = false; }
+    };
+
+    function _hasNoContents() { return _contents === _NULL_CONTENTS; };
+
+    function _putContents(contents) { _contents = contents; };
+
+    function _takeContents() {
+        CALLBACK(_contents);
+        _clearContents();
+    };
+
+    function _clearContents() { _contents = _NULL_CONTENTS; };
+
+    this.isLocked = function() { return _isLocked; };
+
+    this.lock = function() { _isLocked = true; };
+
+    this.tryPutContents = function(contents) {
+        if (this.isLocked()) return ERRBACK(_MSG_LOCKED);
+        if (_hasNoContents()) return _putContents(contents);
+        ERRBACK(_MSG_HAS_CONTENTS);
+    };
+
+    this.tryTakeContents = function() {
+        if (this.isLocked()) return ERRBACK(_MSG_LOCKED);
+        _hasNoContents() ? ERRBACK(_MSG_NO_CONTENTS) : _takeContents();
+    };
+
+    this.tryUnlock = function(key) {
+        if (_protected.isCorrectKey(key)) return _protected.unlock();
+        ERRBACK(_MSG_INCORRECT_KEY);
+    };
+
+    LockableContainerUnitTest(this);
+
+    return _protected;
+
+};
