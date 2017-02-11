854b52f2bc7caffe300f2f372ae5a5b5fcb50155
Double-X <lkllun@yahoo.com.hk>
2017-02-11 21:12:25 +0800

    Create NullLock.java

---
+++ java/locks/NullLock.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doublex.lib.locks;
+
+/**
+ *
+ * @author kenneth.lau
+ * @param <K>
+ */
+public final class NullLock<K> implements ILockable<K> {
+
+    @Override
+    public final boolean isLocked() {
+        return false;
+    }
+
+    @Override
+    public final void lock() {
+    }
+
+    @Override
+    public final void tryUnlock(final K key) {
+    }
+
+}
