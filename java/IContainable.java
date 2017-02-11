c100ebe51b45323aea84990743f9ad556856bf67
Double-X <lkllun@yahoo.com.hk>
2017-02-11 21:08:34 +0800

    Create IContainable.java

---
+++ java/IContainable.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doublex.lib.lockableContainers;
+
+/**
+ *
+ * @author kenneth.lau
+ * @param <C>
+ */
+public interface IContainable<C> {
+
+    void tryPutContents(final C contents);
+
+    C tryTakeContents();
+
+};
