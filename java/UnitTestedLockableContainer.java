0c89cae46fee92d99062b27f185af734cc084275
Double-X <lkllun@yahoo.com.hk>
2017-02-11 21:09:28 +0800

    Create UnitTestedLockableContainer.java

---
+++ java/UnitTestedLockableContainer.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doublex.lib.lockableContainers;
+
+/**
+ *
+ * @author kenneth.lau
+ * @param <C>
+ * @param <K>
+ */
+public final class UnitTestedLockableContainer<C, K> {
+
+    private final LockableContainer<C, K> mLockableContainer;
+
+    public UnitTestedLockableContainer(
+            final LockableContainer<C, K> lockableContainer) {
+        mLockableContainer = lockableContainer;
+    }
+
+    public final void tryPutContents(final C contents) {
+        System.out.println(
+                "UnitTestedLockableContainer tryPutContents contents: " + 
+                        contents);
+        mLockableContainer.tryPutContents(contents);
+    }
+
+    public final C tryTakeContents() {
+        final C contents = mLockableContainer.tryTakeContents();
+        System.out.println("UnitTestedLockableContainer tryTakeContents");
+        if (mLockableContainer.mLocks.isLocked() && contents != null) {
+            System.out.println("Failed! Actual value: " + contents);
+        } else {
+            System.out.println(
+                    "At least nothing can be taken from a locked container!");
+        }
+        return contents;
+    }
+
+}
