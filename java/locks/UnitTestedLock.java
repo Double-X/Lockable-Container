00b5dc2697425fc88ec97bf1f35f0dad427165a2
Double-X <lkllun@yahoo.com.hk>
2017-02-14 21:02:04 +0800

    Update UnitTestedLock.java

--- java/locks/UnitTestedLock.java
+++ java/locks/UnitTestedLock.java
@@ -5,12 +5,18 @@
  */
 package doublex.lib.locks;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author DoubleX
  * @param <K>
  */
 public class UnitTestedLock<K> implements ILockable<K> {
+
+    private static final Logger LOG = 
+            Logger.getLogger(UnitTestedLock.class.getName());
 
     private final ILockable<K> mLock;
 
@@ -20,27 +26,34 @@ public class UnitTestedLock<K> implements ILockable<K> {
 
     @Override
     public final boolean isLocked() {
-        System.out.println(
-                "UnitTestedLock isLocked isLocked: " + mLock.isLocked());
+        unitTestIsLocked();
         return mLock.isLocked();
     }
 
     @Override
     public final void lock() {
-        System.out.println("UnitTestedLock lock");
         mLock.lock();
         unitTestLock();
     }
 
     @Override
     public final void tryUnlock(final K key) {
-        System.out.println("UnitTestedLock tryUnlock key: " + key);
+        unitTestTryUnlock(key);
         mLock.tryUnlock(key);
     }
 
+    private void unitTestIsLocked() {
+        LOG.log(Level.INFO, "UnitTestedLock isLocked");
+        LOG.log(Level.INFO, "isLocked: {0}", mLock.isLocked());
+    }
+
     private void unitTestLock() {
-        System.out.println("UnitTestedLock unitTestLock");
-        System.out.println(mLock.isLocked() ? "Passed!" : "Failed!");
+        LOG.log(Level.INFO, "UnitTestedLock unitTestLock");
+        LOG.log(Level.INFO, mLock.isLocked() ? "Passed!" : "Failed!");
+    }
+
+    private void unitTestTryUnlock(final K key) {
+        LOG.log(Level.INFO, "UnitTestedLock tryUnlock key: {0}", key);
     }
 
 }
