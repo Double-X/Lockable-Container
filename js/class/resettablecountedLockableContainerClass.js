3a046ce3d1b7b974c964e084eee35a222a4c13ad
Double-X <lkllun@yahoo.com.hk>
2017-02-15 20:36:56 +0800

    Update resettablecountedLockableContainerClass.js

--- js/class/resettablecountedLockableContainerClass.js
+++ js/class/resettablecountedLockableContainerClass.js
@@ -1,17 +1,16 @@
-function ResettableCountedLockableContainerClass(
-        CALLBACK, ERRBACK, KEY, MAX_KEY_MISMATCH_COUNT) {
+function ResettableCountedLockableContainerClass(KEY, MAX_KEY_MISMATCH_COUNT) {
 
     'use strict';
 
     var _MSG_LOCKED = 'This container is locked';
 
     var _protected = CountedLockableContainerClass.call(
-            this, CALLBACK, ERRBACK, KEY, MAX_KEY_MISMATCH_COUNT);
+            this, KEY, MAX_KEY_MISMATCH_COUNT);
 
     function _resetKeyMismatchCount() { _protected.keyMismatchCount = 0; };
 
-    this.tryResetKeyMismatchCount = function() {
-        this.isLocked() ? ERRBACK(_MSG_LOCKED) : _resetKeyMismatchCount();
+    this.tryResetKeyMismatchCount = function(errback) {
+        this.isLocked() ? errback(_MSG_LOCKED) : _resetKeyMismatchCount();
     };
 
 };
