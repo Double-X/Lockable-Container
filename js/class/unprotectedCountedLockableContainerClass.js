63df6d42ed36c1aa1ff5cac66a2bede992a39111
Double-X <lkllun@yahoo.com.hk>
2017-02-15 20:37:47 +0800

    Update unprotectedCountedLockableContainerClass.js

--- js/class/unprotectedCountedLockableContainerClass.js
+++ js/class/unprotectedCountedLockableContainerClass.js
@@ -1,14 +1,14 @@
 function UnprotectedCountedLockableContainerClass(
-        lockableContainer, CALLBACK, ERRBACK, KEY, MAX_KEY_MISMATCH_COUNT) {
+        lockableContainer, KEY, MAX_KEY_MISMATCH_COUNT) {
 
     'use strict';
 
     var _protected = CountedLockableContainerClass.call(
-            lockableContainer, CALLBACK, ERRBACK, KEY, MAX_KEY_MISMATCH_COUNT);
+            lockableContainer, KEY, MAX_KEY_MISMATCH_COUNT);
 
     function editProtected(protectedProp) {
     	var originalProtected = _protected[protectedProp];
-    	if (!(originalProtected instanceof Function)) return;
+    	if (typeof originalProtected !== 'function') return;
     	_protected[protectedProp] = function() {
     		console.info('UnprotectedLockableContainerClass editProtected');
     		console.info('protectedProp: ' + protectedProp);
