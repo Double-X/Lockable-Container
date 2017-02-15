fe8869b958d358599ab2b24a334b87265fb9a3f0
Double-X <lkllun@yahoo.com.hk>
2017-02-15 20:34:52 +0800

    Update lockableContainerKiss.js

--- js/lockableContainerKiss.js
+++ js/lockableContainerKiss.js
@@ -1,5 +1,5 @@
-function LockableContainerKiss(CALLBACK, ERRBACK, KEY, MAX_KEY_MISMATCH_COUNT, 
-        IS_RESETTABLE, IS_RESETTING) {
+function LockableContainerKiss(KEY, MAX_KEY_MISMATCH_COUNT, IS_RESETTABLE, 
+        IS_RESETTING) {
 
     'use strict';
 
@@ -15,29 +15,29 @@ function LockableContainerKiss(CALLBACK, ERRBACK, KEY, MAX_KEY_MISMATCH_COUNT,
 
     function keyMismatchCount() { return _keyMismatchCount; };
 
-    function tryPutContents(contents) {
-        if (isLocked()) return ERRBACK('This container is locked');
+    function tryPutContents(contents, errback) {
+        if (isLocked()) return errback('This container is locked');
         if (_contents === _NULL_CONTENTS) return _contents = contents;
-        ERRBACK("This container already has contents");
+        errback("This container already has contents");
     };
 
-    function tryTakeContents() {
-        if (isLocked()) return ERRBACK('This container is locked');
-        if (_contents === _NULL_CONTENTS) return ERRBACK(_MSG_NO_CONTENTS);
-        CALLBACK(_contents);
+    function tryTakeContents(callback, errback) {
+        if (isLocked()) return errback('This container is locked');
+        if (_contents === _NULL_CONTENTS) return errback(_MSG_NO_CONTENTS);
+        callback(_contents);
         _contents = _NULL_CONTENTS;
     };
 
-    function tryUnlock(key) {
+    function tryUnlock(key, errback) {
     	if (keyMismatchCount() >= MAX_KEY_MISMATCH_COUNT) {
-    		return ERRBACK(_MSG_MAX_COUNT_REACHED);
+    	    return errback(_MSG_MAX_COUNT_REACHED);
     	}
-        key === KEY ? _isLocked = false : ERRBACK(_MSG_INCORRECT_KEY);
+        key === KEY ? _isLocked = false : errback(_MSG_INCORRECT_KEY);
         if (IS_RESETTING && !isLocked()) return _keyMismatchCount = 0;
         if (!isLocked()) return;
         _keyMismatchCount++;
         if (keyMismatchCount() < MAX_KEY_MISMATCH_COUNT) return;
-        ERRBACK(_MSG_MAX_COUNT_REACHED);
+        errback(_MSG_MAX_COUNT_REACHED);
     };
 
     var API = {
@@ -51,8 +51,8 @@ function LockableContainerKiss(CALLBACK, ERRBACK, KEY, MAX_KEY_MISMATCH_COUNT,
 
     if (IS_RESETTABLE) {
 
-        function tryResetKeyMismatchCount() {
-            if (isLocked()) return ERRBACK('This container is locked');
+        function tryResetKeyMismatchCount(errback) {
+            if (isLocked()) return errback('This container is locked');
             _keyMismatchCount = 0;
         };
 
